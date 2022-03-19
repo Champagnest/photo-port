b27f5d66d41ed964bf0c5847555a13452169f805
Champagnest <mchampagned@protonmail.com>
2022-03-19 12:03:33 -0700

    gallery aspects added

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -16,3 +16,15 @@ describe('About component', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 })
+
+import React from 'react';
+
+function Gallery() {
+  return (
+    <section>
+      <h1>NAME</h1>
+      <p>DESCRIPTION</p>
+    </section>
+  );
+}
+export default Gallery;
